9d7b809656cd8169a6de1033d090a809e70b1721
4kyoune <wangyyputra@gmail.com>
2024-05-24 09:43:43 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -7,9 +6,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta httpEquiv="Cache-Control" content="max-age=200" />
-          <link rel="shortcut icon" href="/logo.ico" />
-          <link rel="icon" href="/logo.ico" type="image/ico" />
-          <link rel="preconnect" href="https://graphql.anilist.co" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" type="image/ico" />
         </Head>
         <body>
           <Main />
